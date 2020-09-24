f4643afa516912bc9f866b458703f02922ae056d
zane-deg <65408840+zane-deg@users.noreply.github.com>
2020-09-24 17:44:42 -0600

    Update rulefmt.go

--- pkg/rulefmt/rulefmt.go
+++ pkg/rulefmt/rulefmt.go
@@ -23,9 +23,9 @@ import (
 	"github.com/prometheus/common/model"
 	yaml "gopkg.in/yaml.v3"
 
-	"github.com/zane-deg/prometheus/pkg/timestamp"
-	"github.com/zane-deg/prometheus/promql/parser"
-	"github.com/zane-deg/prometheus/template"
+	"github.com/prometheus/prometheus/pkg/timestamp"
+	"github.com/prometheus/prometheus/promql/parser"
+	"github.com/prometheus/prometheus/template"
 )
 
 // Error represents semantic errors on parsing rule groups.
